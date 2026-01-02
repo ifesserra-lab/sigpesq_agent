27138b1189a4cd22571c639099e2728721aacebd
Paulo Sergio dos Santos Júnior <paulossjunior@gmail.com>
2026-01-02 12:38:17 -0300

    Fix CI error: add tests dir and refactor strategies

---
+++ tests/test_import.py
@@ -0,0 +1,5 @@
+from agent_sigpesq.services.reports_service import SigpesqReportService
+
+def test_import():
+    """Verify that the package can be imported."""
+    assert SigpesqReportService is not None
